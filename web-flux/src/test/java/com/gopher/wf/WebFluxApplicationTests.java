4a5db446f27c30a64cf1423201ddaa69fdc5a2d3
6c6763 <gopher_lee@163.com>
2020-10-29 12:43:17 +0800

    fix pom,add web-flux

---
+++ web-flux/src/test/java/com/gopher/wf/WebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gopher.wf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
